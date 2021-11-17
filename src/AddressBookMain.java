d5a7cecf161839f964e0fe29a2ac3165e1f3ad04
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 00:07:22 +0530

    Added:Creating Contacts

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -20,8 +20,44 @@ public class AddressBookMain {
         this.pin = pin;
         this.phNumber = phNumber;
     }
-    public AddressBookMain() {
-        System.out.println("Welcome to Address Book Program");//Welcome Message
+
+    public static void main(String[] args) {
+        //Display Message.
+        System.out.println("Welcome to Address Book Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of contacts to add: ");
+        int n = sc.nextInt();
+
+        //Multiple contacts can be created
+        for(int i =0; i<n ; i++)
+        {
+            System.out.println("Enter your first name: ");
+            String firstName = sc.nextLine();
+            System.out.println("Enter your last name: ");
+            String lastName = sc.nextLine();
+            System.out.println("Enter your Address: ");
+            String address = sc.nextLine();
+            System.out.println("Enter your city: ");
+            String city = sc.nextLine();
+            System.out.println("Enter your state: ");
+            String state = sc.nextLine();
+            System.out.println("Enter your city PIN Code: ");
+            String pin = sc.nextLine();
+            System.out.println("Enter your contact number: ");
+            String phNumber = sc.nextLine();
+
+            //Creation of Object - UC2
+            AddressBookMain person = new AddressBookMain(firstName, lastName, address, city, state, pin, phNumber);
+
+            //Displaying the details
+            System.out.println("Details Added. \n");
+            System.out.println("Name: " + person.firstName + " " + person.lastName);
+            System.out.println("Address: " + person.address + ", " + person.city + ", "+ person.state + " - " + person.pin);
+            System.out.println("Contact Number: " + person.phNumber + "\n");
+
+        }// end of for
+        sc.close();
     }
 
 }
+
