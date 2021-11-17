a1247cb4854e146f9352fdea0cbba016194b1a31
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 00:16:33 +0530

    Added:Edit Contact

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class AddressBookMain {
+    public ArrayList<AddressBookMain> contact = new ArrayList<AddressBookMain>();
+    Scanner sc = new Scanner(System.in);
+
     private String firstName;
     private String lastName;
     private String address;
@@ -10,8 +13,7 @@ public class AddressBookMain {
     private String phNumber;
 
     //Constructor
-    public AddressBookMain(String firstName, String lastName, String address, String city, String state, String pin, String phNumber)
-    {
+    public AddressBookMain(String firstName, String lastName, String address, String city, String state, String pin, String phNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -21,43 +23,50 @@ public class AddressBookMain {
         this.phNumber = phNumber;
     }
 
-    public static void main(String[] args) {
-        //Display Message.
-        System.out.println("Welcome to Address Book Program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of contacts to add: ");
-        int n = sc.nextInt();
-
-        //Multiple contacts can be created
-        for(int i =0; i<n ; i++)
-        {
-            System.out.println("Enter your first name: ");
-            String firstName = sc.nextLine();
-            System.out.println("Enter your last name: ");
-            String lastName = sc.nextLine();
-            System.out.println("Enter your Address: ");
-            String address = sc.nextLine();
-            System.out.println("Enter your city: ");
-            String city = sc.nextLine();
-            System.out.println("Enter your state: ");
-            String state = sc.nextLine();
-            System.out.println("Enter your city PIN Code: ");
-            String pin = sc.nextLine();
-            System.out.println("Enter your contact number: ");
-            String phNumber = sc.nextLine();
-
-            //Creation of Object - UC2
-            AddressBookMain person = new AddressBookMain(firstName, lastName, address, city, state, pin, phNumber);
-
-            //Displaying the details
-            System.out.println("Details Added. \n");
-            System.out.println("Name: " + person.firstName + " " + person.lastName);
-            System.out.println("Address: " + person.address + ", " + person.city + ", "+ person.state + " - " + person.pin);
-            System.out.println("Contact Number: " + person.phNumber + "\n");
-
-        }// end of for
-        sc.close();
+    public void addContact()//Adding person details to Address Book
+    {
+        System.out.println("Enter contact details:");
+        System.out.println("first_name:");
+        firstName = sc.next();
+        System.out.println("last_name:");
+        lastName = sc.next();
+        System.out.println("address:");
+        address = sc.next();
+        System.out.println("city:");
+        city = sc.next();
+        System.out.println("state:");
+        state = sc.next();
+        System.out.println("zip:");
+        pin = sc.next();
+        System.out.println("phone_number:");
+        phNumber = sc.next();
     }
 
+    public void editContact()//Edits the details of the contact present in Address Book
+    {
+        System.out.println("Enter the person whose contact needs to be edited");
+        String name = sc.next();
+        for (int j = 0; j < contact.size(); j++) {
+            AddressBookMain address_book = contact.get(j);
+            if (address_book.firstName.equals(name)) {
+                System.out.println("Enter first_name:");
+                address_book.firstName = sc.next();
+                System.out.println("Enter last_name:");
+                address_book.lastName = sc.next();
+                System.out.println("Enter address:");
+                address_book.address = sc.next();
+                System.out.println("Enter city:");
+                address_book.city = sc.next();
+                System.out.println("Enter state:");
+                address_book.state = sc.next();
+                System.out.println("Enter pin:");
+                address_book.pin = sc.next();
+                System.out.println("Enter phone_number:");
+                address_book.phNumber = sc.next();
+
+            }
+        }
+    }
 }
+
 
