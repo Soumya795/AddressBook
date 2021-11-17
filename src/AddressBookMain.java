ba3a8522df769c6457b437c8788596547c41ac88
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-17 23:56:23 +0530

    Added:Welcome To The Address Book problem

---
+++ src/AddressBookMain.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AddressBookMain {
+    public ArrayList<AddressBookMain> contact = new ArrayList<>();
+    public Scanner sc = new Scanner(System.in);
+
+    public AddressBookMain() {
+        System.out.println("Welcome to Address Book Program");//Welcome Message
+    }
+
+}
