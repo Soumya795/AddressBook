c29e92724aa89a86a789fb1ade12a29ebdf3e16d
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-18 00:03:41 +0530

    Added:Welcome To The Address Book Problem

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,9 +1,25 @@
 import java.util.*;
 
 public class AddressBookMain {
-    public ArrayList<AddressBookMain> contact = new ArrayList<>();
-    public Scanner sc = new Scanner(System.in);
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String pin;
+    private String phNumber;
 
+    //Constructor
+    public AddressBookMain(String firstName, String lastName, String address, String city, String state, String pin, String phNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.pin = pin;
+        this.phNumber = phNumber;
+    }
     public AddressBookMain() {
         System.out.println("Welcome to Address Book Program");//Welcome Message
     }
