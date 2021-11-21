004e53b35d01ff07e5da796c140261581294b21b
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-21 22:52:53 +0530

    Added:Refactored Multiple Address Book

--- src/AddressBook/AddressBookMain.java
+++ src/AddressBook/AddressBookMain.java
@@ -1,43 +1,44 @@
-import java.util.*;
+package AddressBook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
-    public static ArrayList<AddressBookMain> contact = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
+    public ArrayList<AddressBookMain> contact = new ArrayList<AddressBookMain>();
+    public Scanner sc = new Scanner(System.in);
 
-    private String firstName;
-    private String lastName;
+    //class members
+    private String first_name;
+    private String last_name;
     private String address;
     private String city;
     private String state;
-    private String pin;
-    private String phNumber;
+    private String zip;
+    private String phone_number;
     private String email;
 
-    //Constructor
     public AddressBookMain() {
         System.out.println("Welcome to Address Book Program");//Welcome Message
     }
 
-
     public AddressBookMain(String first_name, String last_name, String address, String city, String state, String zip, String phone_number, String email) {
-        this.firstName = first_name;
-        this.lastName = last_name;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.address = address;
         this.city = city;
         this.state = state;
-        this.pin = zip;
-        this.phNumber = phone_number;
+        this.zip = zip;
+        this.phone_number = phone_number;
         this.email = email;
-
     }
 
     public void addContact()//Adding person details to Address Book
     {
         System.out.println("Enter contact details:");
         System.out.println("first_name:");
-        firstName = sc.next();
+        first_name = sc.next();
         System.out.println("last_name:");
-        lastName = sc.next();
+        last_name = sc.next();
         System.out.println("address:");
         address = sc.next();
         System.out.println("city:");
@@ -45,12 +46,12 @@ public class AddressBookMain {
         System.out.println("state:");
         state = sc.next();
         System.out.println("zip:");
-        pin = sc.next();
+        zip = sc.next();
         System.out.println("phone_number:");
-        phNumber = sc.next();
+        phone_number = sc.next();
         System.out.println("email:");
         email = sc.next();
-        contact.add(new AddressBookMain(firstName, lastName, address, city, state, pin, phNumber, email));
+        contact.add(new AddressBookMain(first_name, last_name, address, city, state, zip, phone_number, email));
         System.out.println("Added the details\n");
     }
 
@@ -63,11 +64,11 @@ public class AddressBookMain {
         String lastName = sc.next();
         for (int j = 0; j < contact.size(); j++) {
             AddressBookMain address_book = contact.get(j);
-            if (address_book.firstName.equals(firstName) && address_book.lastName.equals(lastName)) {
+            if (address_book.first_name.equals(firstName) && address_book.last_name.equals(lastName)) {
                 System.out.println("Enter first_name:");
-                firstName = sc.next();
+                first_name = sc.next();
                 System.out.println("Enter last_name:");
-                lastName = sc.next();
+                last_name = sc.next();
                 System.out.println("Enter address:");
                 address = sc.next();
                 System.out.println("Enter city:");
@@ -75,9 +76,9 @@ public class AddressBookMain {
                 System.out.println("Enter state:");
                 state = sc.next();
                 System.out.println("Enter zip:");
-                pin = sc.next();
+                zip = sc.next();
                 System.out.println("Enter phone_number:");
-                phNumber = sc.next();
+                phone_number = sc.next();
                 System.out.println("Enter email:");
                 email = sc.next();
             }
@@ -87,14 +88,14 @@ public class AddressBookMain {
 
     public void delete()//Method to delete contact
     {
-        System.out.println("Enter the person whose contact needs to be deleted");
+        System.out.println("Enter the prson whose contact needs to be deleted");
         System.out.println("Enter first Name:");
         String deleteFirstName = sc.next();
         System.out.println("Enter Last Name:");
         String deleteLastName = sc.next();
         for (int j = 0; j < contact.size(); j++) {
             AddressBookMain address_book = contact.get(j);
-            if (address_book.firstName.equals(deleteFirstName) && address_book.lastName.equals(deleteLastName)) {
+            if (address_book.first_name.equals(deleteFirstName) && address_book.last_name.equals(deleteLastName)) {
                 contact.remove(address_book);
                 System.out.println("Contact is deleted");
             } else {
@@ -110,13 +111,13 @@ public class AddressBookMain {
         for (int j = 0; j < contact.size(); j++) {
             AddressBookMain object = contact.get(j);
             //Displaying the details
-            System.out.println("First Name:" + object.firstName);
-            System.out.println("Last Name:" + object.lastName);
+            System.out.println("First Name:" + object.first_name);
+            System.out.println("Last Name:" + object.last_name);
             System.out.println("Address:" + object.address);
             System.out.println("City:" + object.city);
             System.out.println("State:" + object.state);
-            System.out.println("Zip:" + object.pin);
-            System.out.println("Phone Number:" + object.phNumber);
+            System.out.println("Zip:" + object.zip);
+            System.out.println("Phone Number:" + object.phone_number);
             System.out.println("E mail:" + object.email);
         }
     }
